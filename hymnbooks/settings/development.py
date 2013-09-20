fb71bd3b6899d8824dec03171af24cda23b9bafb
Denis Kolokol <denis_kolokol@ukr.net>
2013-09-20 21:04:06 +0200

    update development settings

--- hymnbooks/settings/development.py
+++ hymnbooks/settings/development.py
@@ -3,14 +3,12 @@
 import os.path
 from hymnbooks.settings.base import *
 
-# Go one level up from settings for the prohject root.
+# Go one level up from settings for the project root.
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(ROOT_PATH, 'site_media/')
-MEDIA_URL = 'http://localhost:8000/site_media/'
 
 STATIC_ROOT = os.path.join(ROOT_PATH, 'static/')
-STATIC_URL = 'http://localhost:8000/static/'
 
 STATICFILES_DIRS = ()
 
