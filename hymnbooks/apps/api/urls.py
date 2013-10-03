ba54bf0b90c4c2715a6a61e8c344644fc3c4611f
Denis Kolokol <denis_kolokol@ukr.net>
2013-10-04 01:04:12 +0200

    hook up user resource

--- hymnbooks/apps/api/urls.py
+++ hymnbooks/apps/api/urls.py
@@ -8,6 +8,7 @@ v1_api = Api(api_name='v1')
 v1_api.register(resources.FieldTypeResource())
 v1_api.register(resources.SectionResource())
 v1_api.register(resources.ManuscriptResource())
+v1_api.register(resources.MongoUserResource())
 
 urlpatterns = patterns('',
    (r'^', include(v1_api.urls)),
